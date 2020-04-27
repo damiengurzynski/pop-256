fc1a6eaf9c4265ddc1c52c7e9989e45dfa65d232
Damien Gurzynski <30811486+damiengurzynski@users.noreply.github.com>
2020-04-28 01:57:05 +0200

    Add files via upload

---
+++ jscreen.js
@@ -0,0 +1,96 @@
+class Jscreen
+{
+    constructor(row,col,cellsize)
+    {
+        this.rows = row;
+        this.cols = col;
+        this.csize = cellsize;
+        this.pxheight = this.rows * this.csize;
+        this.pxwidth = this.cols * this.csize;
+        this.dram = [];
+        this.screen = document.createElement("canvas");
+        this.ctx = this.screen.getContext("2d");
+        this.mode = "text";
+        this.chars = [];
+        this.charx = 5;
+        this.chary = 20;
+        this.clearheight = 0;
+    }
+    
+    init()
+    {
+        this.screen.height = this.pxheight;
+        this.screen.width = this.pxwidth;
+        this.screen.style.backgroundColor = "goldenrod";
+        document.body.appendChild(this.screen);
+        this.write();
+
+        for (var i = 0; i < this.rows * this.cols; i++)
+        {
+            this.dram.push(0);
+        }
+    }
+
+    draw(x,y)
+    {
+        if (this.mode == "graph")
+        {
+            var pos = x + (y * this.rows);
+            x *= this.csize;
+            y *= this.csize;
+            if (!this.dram[pos])
+            {
+                this.ctx.fillStyle = "white";
+                this.ctx.fillRect(x,y,this.csize,this.csize);
+                this.dram[pos] = 1;
+            }
+            else
+            {
+                this.ctx.fillStyle = "black";
+                this.ctx.fillRect(x,y,this.csize,this.csize);
+                this.dram[pos] = 0;
+            }
+        }
+    }
+
+    write(letter)
+    {
+        if (this.mode == "text")
+        {
+            this.clear();
+            this.chars.pop();
+            this.chars.push(letter);
+            this.chars.push("_");
+            this.ctx.font = "20px Monospace";
+            this.ctx.fillStyle = "white";
+            this.ctx.fillText(this.chars.join(""), this.charx, this.chary);
+        }
+    }
+
+    jumpline()
+    {
+        this.chars.pop();
+        this.clear();
+        this.ctx.fillText(this.chars.join(""), this.charx, this.chary);
+        this.chary += 25;
+        this.clearheight += 25;
+        this.chars = [];
+        this.write();
+    }
+
+    delchar()
+    {
+        this.chars.pop();
+        this.chars.pop();
+        this.chars.push("_")
+        this.clear();
+        this.ctx.font = "20px Monospace";
+        this.ctx.fillStyle = "white";
+        this.ctx.fillText(this.chars.join(""), this.charx, this.chary);
+    }
+
+    clear()
+    {
+        this.ctx.clearRect(0,this.clearheight,this.pxwidth, this.pxheight);
+    }
+}
---
+++ main.js
@@ -0,0 +1,135 @@
+//global variables
+var dstack = [];
+var rstack = [];
+var pstack = [];
+var rawinput = [];
+var program = [];
+var maxchar = 0;
+var pc = 0;
+
+//screen initialization
+var screen = new Jscreen(256,256,2);
+screen.init();
+
+//sound initialization
+var sound = new Jsound();
+
+
+//ALU and stack operations
+function exec(a, b)
+{
+    if (a == "put")
+        dstack.push(b);
+
+    if (a == "pop")
+        rstack.push(dstack.pop());
+
+    if (a == "pul")
+        dstack.push(rstack.pop());
+
+    if (a == "add")
+        dstack.push(dstack.pop() + dstack.pop());
+
+    if (a == "sub")
+        dstack[dstack.length-2] = dstack[dstack.length-2] - dstack.pop();
+
+    if (a == "mul")
+        dstack.push(dstack.pop() * dstack.pop());
+
+    if (a == "div")
+        dstack[dstack.length-2] = Math.round(dstack[dstack.length-2] / dstack.pop());
+
+    if (a == "jnr")
+        if (dstack[dstack.length-1] != rstack[rstack.length-1])
+            pc = b - 1;
+
+    if (a == "dup")
+        dstack.push(dstack[dstack.length-1]);
+
+    if (a == "dis")
+        screen.draw(dstack[dstack.length-2], dstack[dstack.length-1]);
+
+    if (a == "clr")
+        screen.clear();
+}
+
+//input handling and parsing
+function parse(input)
+{
+    var inp = input.split(" ");
+    inp[1] = parseInt(inp[1]);
+    program.push(inp);
+}
+
+document.addEventListener("keydown", function(e)
+{
+    e.preventDefault();
+    if (e.key == "Enter" && rawinput.length > 0)
+    {
+        rawinput = rawinput.join("");
+        parse(rawinput);
+        rawinput = [];
+        maxchar = 0;
+        screen.jumpline();
+        
+        sound.type = "sine";
+        sound.play(30);
+    }
+    
+    if (e.key == "Backspace" && maxchar >= 0)
+    {
+        rawinput.pop();
+        maxchar--;
+        screen.delchar();
+    }
+    
+    if (e.key == "Control")
+    {
+        sound.type = "sine";
+        screen.clearheight = 0;
+        screen.clear();
+        screen.chary = 20;
+        if (screen.mode == "text")
+        {
+            sound.play(30);
+            setTimeout(e => {sound.play(34)}, 80);
+            setTimeout(e => {sound.play(37)}, 160);
+            screen.mode = "graph";
+            screen.write("_")
+        }
+        else
+        {
+            sound.play(37);
+            setTimeout(e => {sound.play(34)}, 80);
+            setTimeout(e => {sound.play(30)}, 160);
+            screen.mode = "text";
+        }
+        while (pc < program.length)
+        {
+            exec(program[pc][0], program[pc][1]);
+            pc++;
+        }
+        pc = 0;
+        program = [];
+        screen.chars = [];
+        
+    }
+
+    if (maxchar < 7 && (e.keyCode == 32 || e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 65 && e.keyCode <= 90))
+    {
+        screen.write(e.key);
+        rawinput.push(e.key);
+        maxchar++;
+    }
+
+    //debug
+    // console.clear();
+    // console.log(dstack);
+    // console.log(rstack);
+    // console.log(pstack);
+    // console.log(rawinput);
+    // console.log(program);
+    // console.log(pc);
+});
+
+//character display
